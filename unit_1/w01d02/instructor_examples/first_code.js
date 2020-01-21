5bde4fb2a7fdcc1ba4f2a759c224f91074521670
Karolin Rafalski <kr2174@caa.columbia.edu>
2020-01-21 17:00:30 -0500

    add instructor code along

---
+++ unit_1/w01d02/instructor_examples/first_code.js
@@ -0,0 +1,122 @@
+// print a message to the screen
+// console.log('Hello world')
+
+/*
+ This is a multiline comment
+*/
+
+// type type type
+
+// Strings
+// console.log("Hello, World")
+// console.log('Hello, World')
+// console.log(`Hello, World`)
+
+// console.log(100)
+// console.log(1 + 1)
+
+let a = ''
+let b = 'bongos'
+let c = 'get your'
+
+
+a = b
+b = c
+c = a
+
+// console.log(a, b , c)
+
+// console.log('hello' + ' world')
+
+let adjective = 'beautiful'
+adjective = 'crummy'
+
+// console.log('What a ' + adjective + ' day!!')
+
+// console.log('please' + ' squeeze' + ' the' + ' cheese')
+
+[1,2,3,4]
+
+const obj = {
+  school: 'General Assembly',
+  class: 'Software Engineering Immersive'
+}
+
+// console.log(typeof 1)
+// console.log(typeof 'hello')
+// console.log(typeof true)
+
+const password = 'p1234'
+// console.log(password === 'p1234')
+
+const sale = '100'
+
+// console.log(sale == 100)
+// console.log(sale === 100)
+
+const userAge = 11
+
+// console.log(13 <= userAge)
+
+const shoppingCartTotal = 111
+
+// console.log(50 > shoppingCartTotal)
+
+const oldPassword = 'p1234'
+const newPassword = 'password1234'
+//
+// console.log(oldPassword !== newPassword)
+
+// console.log(1 == 1)
+// console.log(1 === 1)
+
+// console.log('beans' === 'Beans')
+
+// order of operations
+// PEMDAS
+// console.log((5 + 5 * 3) === ((5 + 5)* 3))
+// console.log(9 !== false)
+
+// console.log('A' > 'a')
+
+// console.log('Burger King' < 'McDonalds')
+
+// console.log(NaN == NaN)
+// console.log(false === false)
+
+const fanOfCher = true
+const livesInNY = true
+
+// console.log(fanOfCher && livesInNY)
+
+const jacket = 'black'
+// console.log(jacket === 'blue' || jacket === 'black')
+
+// console.log(Boolean(0))
+
+// console.log(!!0)
+
+// console.log('The number is: ' + 1)
+// console.log('The number is: ' + 2)
+
+let num = 990
+
+// while (num <= 1000) {
+//   console.log('The number is: ' + num)
+//   num++
+// }
+//
+// let runProgram = true
+//
+// while (runProgram) {
+//   console.log('program is runninng')
+//   runProgram = false
+// }
+
+// for (let i = 1; i <= 1000; i++) {
+//   console.log('the value of i is: ' + i)
+// }
+
+for (let x = 999; x > 9 x++ ){
+  console.log(x)
+}

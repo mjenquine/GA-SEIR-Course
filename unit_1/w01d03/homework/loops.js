88a78c9c994b93b65e9be6b862634b89ea902be7
Mark Jenquine <mjenquine@gmail.com>
2020-01-22 20:18:56 -0600

    finished loop practice

---
+++ unit_1/w01d03/homework/loops.js
@@ -0,0 +1,15 @@
+// for (var i = 0; i <= 99; i++) {
+//   console.log(i);
+// }
+// for (var i = 99; i >= 0; i--) {
+//   console.log(i);
+// }
+// for (var i = 0; i <= 98; i += 2) {
+//   console.log(i);
+// }
+// for (var i = 81; i >= 26; i--) {
+//   console.log(i);
+// }
+// for (var i = 3; i <= 90; i += 3) {
+//   console.log(i);
+// }

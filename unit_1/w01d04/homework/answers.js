367198be95708e6dcb42ad73e1a660e88fd1e95c
Mark Jenquine <mjenquine@gmail.com>
2020-01-23 18:31:04 -0600

    Fizz Buzz answered

--- unit_1/w01d04/homework/answers.js
+++ unit_1/w01d04/homework/answers.js
@@ -8,10 +8,22 @@
 //// Part 2
 ////////////////////////
 
-for (var i = 1; i <= 20; i++) {
-  console.log(i);
-}
+// for (var i = 1; i <= 20; i++) {
+//   console.log(i);
+// }
+//
+// for (var i = 0; i <= 200; i += 2) {
+//   console.log(i);
+// }
 
-for (var i = 0; i <= 200; i += 2) {
-  console.log(i);
+for (var i = 1; i <= 100; i++) {
+  if ((i % 3 === 0) && (i % 5 === 0)){
+    console.log('FizzBuzz');
+  } else if (i % 3 === 0) {
+    console.log('Fizz');
+  } else if (i % 5 === 0) {
+    console.log('Buzz');
+  } else {
+    console.log(i);
+  }
 }

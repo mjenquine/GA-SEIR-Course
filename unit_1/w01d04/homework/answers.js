aafe2c1425f960b84ba893da8cc7f47fb8dae0c5
Mark Jenquine <mjenquine@gmail.com>
2020-01-23 18:37:57 -0600

    Wild Wild Life Answered

--- unit_1/w01d04/homework/answers.js
+++ unit_1/w01d04/homework/answers.js
@@ -16,14 +16,29 @@
 //   console.log(i);
 // }
 
-for (var i = 1; i <= 100; i++) {
-  if ((i % 3 === 0) && (i % 5 === 0)){
-    console.log('FizzBuzz');
-  } else if (i % 3 === 0) {
-    console.log('Fizz');
-  } else if (i % 5 === 0) {
-    console.log('Buzz');
-  } else {
-    console.log(i);
-  }
-}
+// for (var i = 1; i <= 100; i++) {
+//   if ((i % 3 === 0) && (i % 5 === 0)){
+//     console.log('FizzBuzz');
+//   } else if (i % 3 === 0) {
+//     console.log('Fizz');
+//   } else if (i % 5 === 0) {
+//     console.log('Buzz');
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee[2] = 5001
+console.log(plantee);
+wolfy[3] = 'Gotham City'
+console.log(wolfy);
+dart.push('Hawkins')
+console.log(dart);
+wolfy[0] = 'Gameboy'
+console.log(wolfy);

9628588e9409ddf04cdedac9bb8de3ef9ab15c65
Mark Jenquine <mjenquine@gmail.com>
2020-01-23 18:52:41 -0600

    Yell at the Ninja Turtles answered

--- unit_1/w01d04/homework/answers.js
+++ unit_1/w01d04/homework/answers.js
@@ -28,17 +28,23 @@
 //   }
 // }
 
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-const sharky = ["Sharky", "shark", 20, "Left Coast"]
-const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+//
+// plantee[2] = 5001
+// console.log(plantee);
+// wolfy[3] = 'Gotham City'
+// console.log(wolfy);
+// dart.push('Hawkins')
+// console.log(dart);
+// wolfy[0] = 'Gameboy'
+// console.log(wolfy);
 
-plantee[2] = 5001
-console.log(plantee);
-wolfy[3] = 'Gotham City'
-console.log(wolfy);
-dart.push('Hawkins')
-console.log(dart);
-wolfy[0] = 'Gameboy'
-console.log(wolfy);
+let turtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo']
+
+for (let element of turtles) {
+  console.log(element.toUpperCase());
+}

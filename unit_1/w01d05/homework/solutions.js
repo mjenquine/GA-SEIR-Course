fe64967899c9553025fcb1b73e9902e82fb0a35d
Mark Jenquine <mjenquine@gmail.com>
2020-01-25 10:13:43 -0600

    verbal questions completed

---
+++ unit_1/w01d05/homework/solutions.js
@@ -0,0 +1,34 @@
+//////////////////////////////////////////
+///////////// Verbal Questions
+//////////////////////////////////////////
+
+//1. A paramater is an input that modifies the behavior of a function. An argument is the value of the paramater specicified when the function is invoked
+//2. The main purpose of Return is to use the function return value like you would use a variable value. Consoe.log is a function that will write the argument value on the console
+
+
+//////////////////////////////////////////
+///////////// Palindrome
+//////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function testing () {
+  return 1 * 2
+  console.log('Hi');
+}
+console.log(testing());

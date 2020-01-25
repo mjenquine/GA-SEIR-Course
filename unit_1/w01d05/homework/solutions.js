d6bc59262b5dfdb41f2d8c9919a543f45654f3d2
Mark Jenquine <mjenquine@gmail.com>
2020-01-25 11:01:02 -0600

    sum array commited

--- unit_1/w01d05/homework/solutions.js
+++ unit_1/w01d05/homework/solutions.js
@@ -5,31 +5,25 @@
 //1. A paramater is an input that modifies the behavior of a function. An argument is the value of the paramater specicified when the function is invoked
 //2. The main purpose of Return is to use the function return value like you would use a variable value. Consoe.log is a function that will write the argument value on the console
 
-
 //////////////////////////////////////////
 ///////////// Palindrome
 //////////////////////////////////////////
 
-const checkPalindrome = (str) => {
-  return str.toLowerCase() == str.split('').reverse().join('').toLowerCase()
+// const checkPalindrome = (str) => {
+//   return str.toLowerCase() == str.split('').reverse().join('').toLowerCase()
+// }
+// console.log(checkPalindrome('Radar'));
+// console.log(checkPalindrome('Borscht'));
+
+//////////////////////////////////////////
+///////////// Sum Array
+//////////////////////////////////////////
+
+const sumArray = (arr) => {
+  let sum = 0
+  for (var i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+  return sum
 }
-console.log(checkPalindrome('Radar'));
-console.log(checkPalindrome('Borscht'));
-
-
-
-
-
-
-
-
-
-
-
-
-
-function testing () {
-  return 1 * 2
-  console.log('Hi');
-}
-console.log(testing());
+console.log(sumArray([1, 2, 3, 4, 5, 6]));

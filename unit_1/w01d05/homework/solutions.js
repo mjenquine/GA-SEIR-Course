ff694bf4a81dd7b573bdd3a4212faa9fcca740d6
Mark Jenquine <mjenquine@gmail.com>
2020-01-25 15:09:42 -0600

    prime numbers completed

--- unit_1/w01d05/homework/solutions.js
+++ unit_1/w01d05/homework/solutions.js
@@ -19,11 +19,59 @@
 ///////////// Sum Array
 //////////////////////////////////////////
 
-const sumArray = (arr) => {
-  let sum = 0
-  for (var i = 0; i < arr.length; i++) {
-    sum += arr[i]
+// const sumArray = (arr) => {
+//   let sum = 0
+//   for (var i = 0; i < arr.length; i++) {
+//     sum += arr[i]
+//   }
+//   return sum
+// }
+// console.log(sumArray([1, 2, 3, 4, 5, 6]));
+
+//////////////////////////////////////////
+///////////// Prime Numbers
+//////////////////////////////////////////
+
+const checkPrime = (number) => {
+  if (number === 2) {
+    return true
   }
-  return sum
+  else {
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+  }
+  return true
 }
-console.log(sumArray([1, 2, 3, 4, 5, 6]));
+const printPrimes = (num) => {
+  for (var i = 2; i <= num ; i++) {
+    checkPrime(i)
+    if (checkPrime(i) === true) {
+      console.log(i);
+    }
+  }
+}
+printPrimes(100)
+
+// const checkPrime = (number) => {
+//   if (number === 2) {
+//     return number
+//   }
+//   else {
+//     for (let i = 2; i <= Math.sqrt(number); i++) {
+//       if (number % i === 0) {
+//         return false;
+//       }
+//     }
+//   }
+//   return number
+// }
+// const printPrimes = (num) => {
+//   for (var i = 2; i <= num ; i++) {
+//     checkPrime(i)
+//     console.log(checkPrime(i));
+//   }
+// }
+// printPrimes(100)

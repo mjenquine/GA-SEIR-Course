aec94135fa657cdcbbb734302da006db8fec3d60
Mark Jenquine <mjenquine@gmail.com>
2020-01-28 20:34:10 -0600

    objectception complete

--- unit_1/w02d02/homework/answers.js
+++ unit_1/w02d02/homework/answers.js
@@ -28,13 +28,74 @@
 //              Take it Easy
 ////////////////////////////////////////////
 
-let rainbow = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
-console.log(rainbow[4]);
-const me = {
-  name: 'Mark Jenquine',
-  favoriteFood: 'BBQ',
-  hobby: 'Cycling',
-  location: 'Milwaukee',
-  favoriteDataType: 'Objects'
+// let rainbow = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
+// console.log(rainbow[4]);
+// const me = {
+//   name: 'Mark Jenquine',
+//   favoriteFood: 'BBQ',
+//   hobby: 'Cycling',
+//   location: 'Milwaukee',
+//   favoriteDataType: 'Objects'
+// }
+// console.log(me.hobby);
+
+////////////////////////////////////////////
+//              Crazy Object
+////////////////////////////////////////////
+
+// const crazyObject = {
+//   taco: [{meat: 'steak',
+//          cheese: ['panela', 'queso', 'chihuahua']},
+//          {meat: 'chicken',
+//           salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+//         ],
+//   larry: {
+//     nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+//     quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+//     characters: [{
+//       name: "Jeff",
+//       occupation: "manager"
+//     },
+//     {
+//       name: "funkhauser",
+//       occupation: "tv dude"
+//     },
+//     {
+//       name: "susie",
+//       occupation: "jeffs wife",
+//       favourtieHobby: "Swearing at Larry and Jeff"
+//     },
+//     ]
+//   }
+// }
+// // console.log(crazyObject.taco[1].salsa[5]);
+// // console.log(crazyObject.larry.quotes[0]);
+// // console.log(crazyObject.larry.characters[2].favourtieHobby);
+// // console.log(crazyObject.larry.nicknames[1]);
+// // console.log(crazyObject.larry.characters);
+// // crazyObject.larry.quotes.push('I\'m trying to elevate small talk to medium talk')
+// // console.log(crazyObject.larry.quotes);
+
+////////////////////////////////////////////
+//              Object-ception
+////////////////////////////////////////////
+
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
 }
-console.log(me.hobby);
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo);

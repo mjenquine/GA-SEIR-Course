c591fa5d7b62b7a8b73c828b70c61b76d9f1abc3
Mark Jenquine <mjenquine@gmail.com>
2020-01-28 20:09:34 -0600

    took it easy

---
+++ unit_1/w02d02/homework/answers.js
@@ -0,0 +1,40 @@
+////////////////////////////////////////////
+//              DataTypes
+////////////////////////////////////////////
+// 1. DataTypes boolean
+// const light = true
+// 2. object
+// const User = {
+//   name: "Mark",
+//   email: 'mjenquine@gmail.com'
+// }
+// 3. Array
+// let spaceship = ['hull', 'laser blasters', 'tractor beam', 'warp drive']
+// 4. Array
+// let names = ['John', 'Mark', 'Ben', 'Vikram']
+// 5. Object
+// const students = {
+//   {name: 'John', Location: 'Austin'},
+//   {name: 'Mark', Location: 'Milwaukee'},
+//   {name: 'Ben', Location: 'Colorado'}
+// }
+// 6. Object Array
+// const students = {
+//   {name: 'John', Location: 'Austin', favoriteMovies: ['Ant Man', 'Star Trek', 'Spiderman']},
+//   {name: 'Mark', Location: 'Milwaukee', favoriteMovies: ['Momento', 'Star Wars', 'Benji']},
+//   {name: 'Ben', Location: 'Colorado', favoriteMovies: ['Snatch', 'Saving Private Ryan', 'Sandlot']}
+
+////////////////////////////////////////////
+//              Take it Easy
+////////////////////////////////////////////
+
+let rainbow = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
+console.log(rainbow[4]);
+const me = {
+  name: 'Mark Jenquine',
+  favoriteFood: 'BBQ',
+  hobby: 'Cycling',
+  location: 'Milwaukee',
+  favoriteDataType: 'Objects'
+}
+console.log(me.hobby);

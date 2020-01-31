344c0c31b08b3a32aa5bc0391cf824bc4f8d3e81
Mark Jenquine <mjenquine@gmail.com>
2020-01-30 20:44:37 -0600

    created our pizza rat

--- unit_1/w02d04/homework/donut_adventure/adventure.js
+++ unit_1/w02d04/homework/donut_adventure/adventure.js
@@ -18,7 +18,33 @@ class Hero {
     console.log('i\'m ready to rumble');
   }
 }
-const earl = new Hero ('Earl')
-// earl.talkSass()
-// earl.announceHealth()
-// earl.fight()
+const dougie = new Hero ('Dougie')
+// dougie.talkSass()
+// dougie.announceHealth()
+// dougie.fight()
+
+class Enemy {
+  constructor (name, ) {
+    this.name = name
+    this.health = 100
+    this.weapons = {
+      pepperoniStars: 5,
+      cheeseGrease: 10
+    }
+    this.catchPhrases = ['i\'m youtube famous', 'i\'m more dangerous than an uncovered sewer']
+  }
+  talkSmack () {
+    console.log(this.catchPhrases[Math.floor(this.catchPhrases.length * Math.random())]);
+  }
+  announceHealth () {
+    console.log(this.health);
+  }
+  fight () {
+    console.log('i\'m going to flatten you like a slice of pepperoni');
+  }
+}
+const pizzaRat = new Enemy ('Pizza Rat')
+console.log(pizzaRat);
+pizzaRat.talkSmack()
+pizzaRat.announceHealth()
+pizzaRat.fight()

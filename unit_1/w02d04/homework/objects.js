dbd77050d30cbae2a3da2d855e55ceb4ee5e55a7
Mark Jenquine <mjenquine@gmail.com>
2020-01-30 19:51:10 -0600

    created Timmy's story

--- unit_1/w02d04/homework/objects.js
+++ unit_1/w02d04/homework/objects.js
@@ -19,3 +19,71 @@ const gus = new Hamster ('Timmy', 'Gus')
 // gus.wheelRun();
 // gus.eatFood();
 // console.log(gus.getPrice());
+
+class Person {
+  constructor (name) {
+    this.name = name
+    this.age = 0
+    this.height = 0
+    this.weight = 0
+    this.mood = 0
+    this.hamsters = []
+    this.bankAccount = 0
+  }
+  getName () {
+    return this.name
+  }
+  getAge () {
+    return this.age
+  }
+  getWeight () {
+    return this.weight
+  }
+  greet () {
+    console.log(`Hello ${this.weight}`); //need to find the name that goes in here
+  }
+  eat () {
+    this.weight++
+    this.mood++
+  }
+  exercise () {
+    this.weight--
+  }
+  ageUp () {
+    this.age++
+    this.height++
+    this.weight++
+    this.mood--
+    this.bankAccount += 10
+  }
+  buyHamster (hamsterName) {
+    this.hamsters.push(hamsterName)
+    this.mood += 10
+    this.bankAccount -= hamsterName.price
+  }
+}
+const timmy = new Person ('Timmy')
+
+for (let i = 0; i < 5; i++) {
+  timmy.ageUp()
+}
+for (let i = 0; i < 5; i++) {
+  timmy.eat()
+}
+for (let i = 0; i < 5; i++) {
+  timmy.exercise()
+}
+for (let i = 0; i < 9; i++) {
+  timmy.ageUp()
+}
+timmy.buyHamster(gus);
+for (let i = 0; i < 15; i++) {
+  timmy.ageUp()
+}
+for (let i = 0; i < 2; i++) {
+  timmy.eat()
+}
+for (let i = 0; i < 2; i++) {
+  timmy.exercise()
+}
+console.log(timmy);

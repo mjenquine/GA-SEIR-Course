180f75694d9ddee2a2f03d1f4bda9b7743f04f35
Mark Jenquine <mjenquine@gmail.com>
2020-02-02 09:11:41 -0600

    moved space battle to homework dir

---
+++ unit_1/w02d05/homework/spacebattle/app.js
@@ -0,0 +1,127 @@
+///////////////////////////////////////////////////
+//randomizes our alien values
+///////////////////////////////////////////////////
+const rng = (arr) => {
+  let rng = Math.floor(Math.random() * arr.length)
+  return arr[rng];
+}
+
+
+///////////////////////////////////////////////////
+//these arrays are full of ships
+///////////////////////////////////////////////////
+let alienShipsArray = []
+let spaceShipsArray = []
+
+
+///////////////////////////////////////////////////
+//spaceship class
+///////////////////////////////////////////////////
+class SpaceShip {
+  constructor () {
+    this.hull = 20;
+    this.firepower = 5;
+    this.accuracy = .7;
+  }
+  attack (player, enemy) {
+    let rng = Math.random();
+    if(rng <= this.accuracy){
+      enemy.hull -= player.firepower;
+    }
+  }
+}
+
+
+///////////////////////////////////////////////////
+//alienship class
+///////////////////////////////////////////////////
+class AlienShip {
+  constructor () {
+    this.hull = rng([3, 4, 5, 6]);
+    this.firepower = rng([2, 3, 4]);
+    this.accuracy = rng([.6, .7, .8]);
+  }
+  attack (player, enemy) {
+    let rng = Math.random();
+    if(rng <= this.accuracy){
+      player.hull -= enemy.firepower;
+    }
+  }
+}
+
+
+///////////////////////////////////////////////////
+//create our ships
+///////////////////////////////////////////////////
+const buildAlienShipArray = () => {
+  for (var i = 1; i <= 6; i++) {
+    alienShipsArray.push(alienship = new AlienShip())
+  }
+}
+
+spaceShipsArray.push(spaceShip = new SpaceShip());
+// console.log(spaceShipsArray);
+// console.log(alienShipsArray);
+
+
+///////////////////////////////////////////////////
+//This will start the game
+///////////////////////////////////////////////////
+playGame = () => {
+
+}
+///////////////////////////////////////////////////
+//       Question / Response Prompt
+///////////////////////////////////////////////////
+// askForAction = (player, enemy) => {
+//   let choice = prompt ('Alien droids are approaching fast. What would you like to do?', '(a)ttack / (r)etreat')
+//   if (!choice) {
+//     alert('I\'m sorry. Try again')
+//     return
+//   }
+//   if (choice === 'a') {
+//     player.attack(player, enemy);
+//     checkHealth(player, enemy)
+//     //askForAction(player, enemy);
+//   }
+//   if (choice === 'r') {
+//     alert('You can\'t run forever. They\'re too fast')
+//     askForAction(player, enemy);
+//   }
+//   else {
+//     alert ('You\'d better act fast. They\'re approaching quickly')
+//     askForAction(player, enemy);
+//   }
+// }
+
+///////////////////////////////////////////////////
+//     Check Health of Both Player and Enemy
+///////////////////////////////////////////////////
+
+const checkHealth = (player, enemy) => {
+  if (enemy.hull <= 0) {
+    shiftAlienArray(player, enemy)
+    alert('You\'ve destroyed an alien ship')
+  }
+}
+
+
+////////////////////////////////////////////////////
+//    Shift Alien Array
+///////////////////////////////////////////////////
+
+const shiftAlienArray = () => {
+  for (var i = 0; i < 1; i++) {
+    alienShipsArray.shift()
+  }
+}
+
+
+
+
+
+
+
+buildAlienShipArray();
+shiftAlienArray();
+askForAction(spaceShipsArray[0], alienShipsArray[0])

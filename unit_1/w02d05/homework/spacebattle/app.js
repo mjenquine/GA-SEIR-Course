c646b23a0eeb7691d6031eb20f71f0b7f5f1cac4
Mark Jenquine <mjenquine@gmail.com>
2020-02-02 09:46:57 -0600

    added game logic

--- unit_1/w02d05/homework/spacebattle/app.js
+++ unit_1/w02d05/homework/spacebattle/app.js
@@ -68,11 +68,11 @@ spaceShipsArray.push(spaceShip = new SpaceShip());
 //This will start the game
 ///////////////////////////////////////////////////
 playGame = () => {
-  alert('Welcome Captain. You made it to the flight deck just in time. There seems to be hostile foriegn objects on the radar ahead. There heading in our direction at an alarming rate')
+  alert('Welcome Captain. You made it to the flight deck just in time. There seems to be hostile foriegn objects on the radar ahead. They\'re heading in our direction at an alarming rate of speed.')
   askForAction(spaceShipsArray[0], alienShipsArray[0])
 }
 /////////////////////////////////////////////////
-      Question / Response Prompt
+//      Question / Response Prompt
 /////////////////////////////////////////////////
 askForAction = (player, enemy) => {
   let choice = prompt ('Alien droids are approaching fast. What would you like to do?', '(a)ttack / (r)etreat')
@@ -82,8 +82,7 @@ askForAction = (player, enemy) => {
   }
   if (choice === 'a') {
     player.attack(player, enemy);
-    checkHealth(player, enemy)
-    //askForAction(player, enemy);
+    checkEnemyHealth(player, enemy)
   }
   if (choice === 'r') {
     alert('You can\'t run forever. They\'re too fast')
@@ -98,10 +97,11 @@ askForAction = (player, enemy) => {
 //     Check Health of Enemy
 ///////////////////////////////////////////////////
 
-const checkHealth = (player, enemy) => {
+const checkEnemyHealth = (player, enemy) => {
   if (enemy.hull <= 0) {
     shiftAlienArray()
     alert('You\'ve destroyed an alien ship')
+    checkForWin(player, enemy)
   }
 }
 
@@ -115,11 +115,28 @@ const shiftAlienArray = () => {
   }
 }
 
+////////////////////////////////////////////////////
+//   Check for Win
+////////////////////////////////////////////////////
+
+const checkForWin = (player, enemy) => {
+  if (alienShipsArray.length === 0) {
+    alert('Captian, you\'ve destroyed all alien vessels. You\'ve saved the fleet!!!'')
+    return
+  } else {
+    alert('There\'s more alien ships on the radar. Reload sonic blast canons')
+    askForAction(player, enemy)
+  }
+}
+
+
+
+
 ///////////////////////////////////////////////////
 //     Check Health of Player
 ///////////////////////////////////////////////////
 
-const checkHealth = (player, enemy) => {
+const checkPlayerHealth = (player, enemy) => {
   if (player.hull <= 0) {
     alert('You\'ve been destroyed by an alien ship!!! Game Over')
     return

db03c3ad480a362ab8497d257e9ace40ddd296a7
Mark Jenquine <mjenquine@gmail.com>
2020-02-03 18:32:52 -0600

    Commit 3 - I added list elements. That code didn't seem very dry. I wonder if there is something I could do about it in the Hungry for More section

--- unit_1/w03d01/homework/hogwarts/app.js
+++ unit_1/w03d01/homework/hogwarts/app.js
@@ -25,6 +25,47 @@ const $h4Wand = $('<h4>')
 $h4Wand.text('Holly Wand with Unicorn Hair Core')
 $container.append($h4Wand)
 
+/////////////////////////////////////////
+//           Year Three
+////////////////////////////////////////
+
+const $ul = $('<ul>')
+$ul.attr('storage', 'trunk')
+$container.append($ul)
+
+const $li1 = $('<li>')
+const $li2 = $('<li>')
+const $li3 = $('<li>')
+const $li4 = $('<li>')
+const $li5 = $('<li>')
+const $li6 = $('<li>')
+
+ $li1.text('butter beer')
+ $li2.text('invisibility cloak')
+ $li3.text('magic map')
+ $li4.text('time turner')
+ $li5.text('leash')
+ $li6.text('Bertie Bott\'s Every Flavor [Jelly] Beans')
+
+$li2.addClass('secret')
+$li3.addClass('secret')
+$li4.addClass('secret')
+$li5.addClass('cat')
+
+$ul.append($li1)
+$ul.append($li2)
+$ul.append($li3)
+$ul.append($li4)
+$ul.append($li5)
+$ul.append($li6)
+
+
+
+
+
+
+
+
 
 
 

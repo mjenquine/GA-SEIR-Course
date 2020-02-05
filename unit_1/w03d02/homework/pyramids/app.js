0f527080fc05bdce7b49a9ff44781be9a8815422
Mark Jenquine <mjenquine@gmail.com>
2020-02-04 23:29:30 -0600

    pyramid setup

---
+++ unit_1/w03d02/homework/pyramids/app.js
@@ -0,0 +1,52 @@
+$(() => {
+//////////////////////////////////////////////////////////////////////////////
+
+  const makePyramid = () => {
+    for (let i = 0; i < 1; i++) {
+      if (true) {
+        const $makeTriangle = $('<div>').addClass('triangle')
+        $('body').append($makeTriangle)
+      }
+      if (true) {
+        const $makeTriangle = $('<div>').addClass('triangle')
+        $('body').append($makeTriangle)
+      }
+      if (true) {
+        const $makeTriangle = $('<div>').addClass('triangle')
+        $('body').append($makeTriangle)
+      }
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+makePyramid()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+});

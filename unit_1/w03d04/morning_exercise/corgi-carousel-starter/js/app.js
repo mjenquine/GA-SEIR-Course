c4c9efb4743b6e1f96b19c6a6117f48fd4f76854
Mark Jenquine <mjenquine@gmail.com>
2020-02-06 11:38:54 -0600

    morning notes

--- unit_1/w03d04/morning_exercise/corgi-carousel-starter/js/app.js
+++ unit_1/w03d04/morning_exercise/corgi-carousel-starter/js/app.js
@@ -1,1 +1,29 @@
-console.log('corgi carousel connected to app.js!');
+
+
+$(() => {
+//=======================================================================
+  let currentImageIndex = 0
+  let highestIndex = $('.carousel-images').children().length -1
+
+  $('.next').on('click', () => {
+    // stuff is going to go here
+    $('.carousel-images').children().eq(currentImageIndex).css('display', 'none')
+    if (currentImageIndex < highestIndex) {
+      currentImageIndex++
+    } else {
+      currentImageIndex = 0
+    }
+    $('.carousel-images').children().eq(currentImageIndex).css('display', 'block')
+  })
+
+  $('.previous').on('click', () => {
+    $('.carousel-images').children().eq(currentImageIndex).css('display', 'none')
+    if (currentImageIndex > 0) {
+      currentImageIndex--
+    } else {
+      currentImageIndex = highestIndex
+    }
+    $('.carousel-images').children().eq(currentImageIndex).css('display', 'block')
+  })
+//=======================================================================
+})
--- unit_1/w03d04/student_examples/palette_picker/app.js
+++ unit_1/w03d04/student_examples/palette_picker/app.js
@@ -1,1 +1,43 @@
-console.log('app.js loaded for palette picker');
+
+const $colorPalette = $('#color-palette')
+
+const $myPalette = $('#my-palette')
+
+const $generate = $('#generate')
+
+const makePalette = () => {
+  $colorPalette.empty()
+  for (let i = 0; i < 150; i++) {
+    const red = Math.floor(Math.random() * 256)
+    const blue = Math.floor(Math.random() * 256)
+    const green = Math.floor(Math.random() * 256)
+    const color = 'rgb(' + red + ',' + blue + ',' + green + ')'
+
+    const $square = $('<div>')
+    $square.addClass('square')
+    $square.css('background-color', color)
+    $square.on('click', addColor)
+    $colorPalette.append($square)
+  }
+}
+
+const addColor = () => {
+  const color = $(event.currentTarget).css('background-color')
+  const $square = $('<div>').addClass('square').css('background-color', color).appendTo($myPalette)
+}
+
+makePalette()
+
+$generate.on('click', makePalette)
+
+
+
+
+
+
+
+
+
+
+// my refactor of makePalette is below
+// const $square = $('<div>').addClass('square').appendTo($colorPalette)

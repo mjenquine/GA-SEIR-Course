5bae6b9b7a5857f947cc151f1f755d268d7554be
Mark Jenquine <mjenquine@gmail.com>
2020-02-15 10:22:05 -0600

    tip calculator express application created

--- unit_2/w04d05/homework/routes_hw/server.js
+++ unit_2/w04d05/homework/routes_hw/server.js
@@ -6,6 +6,12 @@ app.get('/greetings/:name', (req, res) => {
   res.send('Well hi there ' + req.params.name)
 })
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+  let tip = (Number(req.params.tipPercentage) / 100) * Number(req.params.total)
+  console.log(tip);
+  res.send('The total tip is ' + tip)
+})
+
 
 
 

6eb0890d751dcf319cd2af5368ba7c8f4808955e
Karolin Rafalski <kr2174@caa.columbia.edu>
2020-02-17 13:43:57 -0500

    updates to morning, syntax, typo fixes

--- unit_2/w05d02/student_labs/mars_mission/server.js
+++ unit_2/w05d02/student_labs/mars_mission/server.js
@@ -1,6 +1,6 @@
 // DEPENDENCIES
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 // run `npm i` to install dependencies in package.json
 
@@ -18,47 +18,51 @@ const app = express();
 // views/missions folder has not been created create it!
 
 // PORT
-const port = 3000;
+const PORT = 3000
 
 // DATA - put into marsMissions.js file inside of a models folder, for module.exports
 // remember to require it in the server
 const marsMissions = [
   {
-    name: "Curiosity",
-    launchDate: "26 Nov 2011",
-    operator: "NASA",
-    missionType: "Rover",
-    img: "https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/pia19808-main_tight_crop-monday.jpg"
+    name: 'Curiosity',
+    launchDate: '26 Nov 2011',
+    operator: 'NASA',
+    missionType: 'Rover',
+    img:
+      'https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/pia19808-main_tight_crop-monday.jpg'
   },
   {
-    name: "Opportunity",
-    launchDate: "8 Jul 2003",
-    operator: "NASA",
-    missionType: "Rover",
-    img: "https://www.jpl.nasa.gov/images/mer/20091207/mer20091207-browse.jpg"
+    name: 'Opportunity',
+    launchDate: '8 Jul 2003',
+    operator: 'NASA',
+    missionType: 'Rover',
+    img: 'https://www.jpl.nasa.gov/images/mer/20091207/mer20091207-browse.jpg'
   },
   {
-    name: "Spirit",
-    launchDate: "10 Jun 2003",
-    operator: "NASA",
-    missionType: "Rover",
-    img: "https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/images/287330main_image_1215_full.jpg"
+    name: 'Spirit',
+    launchDate: '10 Jun 2003',
+    operator: 'NASA',
+    missionType: 'Rover',
+    img:
+      'https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/images/287330main_image_1215_full.jpg'
   },
   {
-    name: "Sojourner",
-    launchDate: "4 Dec 1996",
-    operator: "NASA",
-    missionType: "Rover",
-    img: "https://img.purch.com/w/640/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzAwOS84NjAvaTAyL21hcnMtcGF0aGZpbmRlci1zb2pvdXJuZXIuanBnPzEzMDYzNTk5Nzg="
+    name: 'Sojourner',
+    launchDate: '4 Dec 1996',
+    operator: 'NASA',
+    missionType: 'Rover',
+    img:
+      'https://img.purch.com/w/640/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzAwOS84NjAvaTAyL21hcnMtcGF0aGZpbmRlci1zb2pvdXJuZXIuanBnPzEzMDYzNTk5Nzg='
   },
   {
-    name: "Rosetta",
-    launchDate: "2 Mar 2004",
-    operator: "ESA",
-    missionType: "Gravity Assist",
-    img: "https://www.jpl.nasa.gov/spaceimages/images/largesize/PIA18891_hires.jpg"
+    name: 'Rosetta',
+    launchDate: '2 Mar 2004',
+    operator: 'ESA',
+    missionType: 'Gravity Assist',
+    img:
+      'https://www.jpl.nasa.gov/spaceimages/images/largesize/PIA18891_hires.jpg'
   }
-];
+]
 
 // INDEX Route
 // send data to 'missions/index.ejs' view
@@ -71,9 +75,7 @@ const marsMissions = [
 // the view should have a link back to the index
 // Stretch: display the image inside an image tag
 
-
-
 // LISTENER
-app.listen(port, ()=> {
-  console.log('Missions to Mars running on port: ', port);
+app.listen(PORT, () => {
+  console.log('Missions to Mars running on port: ', PORT)
 })

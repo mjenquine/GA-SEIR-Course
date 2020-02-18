353b9b4f8b76e094aa80dd732a8c69ed0c3d740c
Mark Jenquine <mjenquine@gmail.com>
2020-02-18 16:27:17 -0600

    app.get displaying res.send(drinks) in the browser as JSON

--- unit_2/w05d02/homework/gitpub/server.js
+++ unit_2/w05d02/homework/gitpub/server.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const drinks = require('./models/drinks.js')
 
 app.get('/', (req, res) => {
   res.send('Welcome to the gitpub app!')
 })
 
-
+app.get('/drinks', (req, res) => {
+  res.send(drinks)
+})
 
 
 

6f9f788f1c8469fb80f361484357bf62fa9e6f4c
Mark Jenquine <mjenquine@gmail.com>
2020-02-18 20:30:48 -0600

    show food is rendering

--- unit_2/w05d02/homework/gitpub/server.js
+++ unit_2/w05d02/homework/gitpub/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const drinks = require('./models/drinks.js')
+const food = require('./models/food.js')
 
 app.get('/', (req, res) => {
   res.send('Welcome to the gitpub app!')
@@ -9,12 +10,19 @@ app.get('/', (req, res) => {
 
 app.get('/drinks', (req, res) => {
   res.render('index.ejs', {
-    allDrinks: drinks
+    allDrinks: drinks,
+    allFood: food
   })
 })
 app.get('/drinks/:id', (req, res) => {
   res.render('show.ejs', {
-    allDrinks: drinks[req.params.id]
+    allDrinks: drinks[req.params.id],
+    allFood: food[req.params.id]
+  })
+})
+app.get('/food/:id', (req, res) => {
+  res.render('showfood.ejs', {
+    allFood: food[req.params.id]
   })
 })
 

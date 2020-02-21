9997be34f6ad7f3d81b181e028d64a8270b2cfe2
Mark Jenquine <mjenquine@gmail.com>
2020-02-20 21:20:34 -0600

    First commit. Project almost complete

---
+++ unit_2/w05d04/student_labs/pokedex/server.js
@@ -0,0 +1,79 @@
+// dependancies
+const express = require('express');
+const app = express();
+const methodOverride = require('method-override')
+const PORT = 3000
+// middleware
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+
+// data
+const pokemon = require('./models/pokemon.js')
+
+// INDEX
+app.get('/pokemon', (req, res) => {
+res.render('index.ejs', { pokemon: pokemon })
+})
+//NEW
+app.get('/pokemon/new', (req, res) => {
+  res.render('new.ejs')
+})
+
+// SHOW
+app.get('/pokemon/:index', (req, res) => {
+  const currentPokemon = pokemon[req.params.index]
+  res.render('show.ejs', {
+  pokemon: currentPokemon })
+});
+//EDIT
+app.get('/pokemon/:index/edit', (req, res) => {
+  res.render('edit.ejs', {
+    pokemon:  pokemon[req.params.index],
+    index: req.params.index
+  })
+})
+// Update
+app.put('/pokemon/:index', (req, res) => {
+  pokemon[req.params.index] = req.body
+  req.body.stats = {
+    hp: req.body.hp,
+    attack: req.body.attack,
+    defense: req.body.defense
+  }
+  console.log(req.body);
+  res.redirect('/pokemon')
+})
+//CREATE
+app.post('/pokemon', (req, res) => {
+  req.body.stats = {
+    hp: req.body.hp,
+    attack: req.body.attack,
+    defense: req.body.defense
+  }
+  //console.log(req.body);
+  pokemon.push(req.body)
+  res.redirect('/pokemon')
+})
+
+
+app.listen(PORT, () => {
+  console.log("listening");
+})
+
+// {
+//   name: 'mj',
+//   type: 'grass',
+//   hp: '1',
+//   attack: '1',
+//   defense: '1'
+// }
+// {
+//   name: 'mj',
+//   type: 'grass',
+//   stats: {
+//     hp: '1',
+//     attack: '1',
+//     defense: '1'
+//   }
+// }

695aa4e89e5b3a62111e9c77502c2f0f2fb1e9fb
Ayla Ex <aylaex01@gmail.com>
2020-02-21 15:01:35 -0500

    pushing up homework

---
+++ unit_2/w05d05/homework/mongo_burgers/answers.js
@@ -0,0 +1,70 @@
+// create 5 burgers (at least 3 should be beef)
+
+
+
+// find all the burgers
+
+
+// show just the meat of each burger
+
+
+// show just the toppings of each burger
+
+
+// show everything but the cheese
+
+
+// find all the burgers with beef
+
+
+// find all the burgers that are not beef
+
+
+// find the first burger with cheese
+
+
+// find one and update the first burger with cheese to have a property of 'double cheese'
+
+
+// find the burger you updated to have double cheese
+
+
+// find and update all the beef burgers to be 'veggie'
+
+
+// delete one of your veggie burgers
+// WRONG - dELETES ALL : db.burger.remove({meat: 'veggie'})
+
+
+// drop the collection
+//Expected Output
+//true
+
+// drop the database
+//Expected Output
+// {
+//   "dropped": "burgers",
+//   "ok": 1
+// }
+
+
+//
+// Bonus
+//recreate your burgers database and your burger collection
+//copy paste your insert burgers from above to reseed your database
+
+
+//
+// Change the name of the key cheese to 'pumpkinSpice'
+
+
+// find all the burgers with ketchup (or another topping you used at least once)
+
+
+// find all the burgers with pickles (or a topping you used more than once) and remove the pickles
+
+
+// add a topping of 'eggs' to all the beef burgers
+//note since this db is 'reset' there should be no veggie burgers, all beef burgers should still be intact
+
+//Add a price to each burger, start with $5.00 for each burger 

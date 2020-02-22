a19d968dc329761afabc3f486f4fa2d4b8f26c66
Mark Jenquine <mjenquine@gmail.com>
2020-02-22 16:30:31 -0600

    building hotel schema

--- unit_2/w05d05/homework/hotels_app/app.js
+++ unit_2/w05d05/homework/hotels_app/app.js
@@ -1,3 +1,18 @@
 //dependancies
 const mongoose = require('mongoose')
-const Hotels = require('./models/seed.js')
+const Hotel = require('./models/hotel.js')
+const hotelSeed = require('./models/hotel.js')
+const db = mongoose.connection
+mongoose.Promise = global.Promise
+//config
+const mongoURI = 'mongodb://localhost:27017/hotel'
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+},
+() => {
+  console.log('the connection with mongod is established');
+})
+db.on('error', err => console.log(err.message + ' is mongod not running?'))
+db.on('connected', () => console.log('mongo connected: ', mongoURI))
+db.on('disconnected', () => console.log('mongo disconnected'))
---
+++ unit_2/w05d05/homework/hotels_app/models/hotel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const hotelSchema = new Schema (
+  {
+
+
+
+  }
+  {timestamps: true}
+)
+
+const Hotel = mongoose.model('Hotel', tweetSchema)
+
+module.export = Hotel

f06a97ded377ed3a681fa65c5f23d3359ed29062
Mark Jenquine <mjenquine@gmail.com>
2020-02-22 16:58:04 -0600

    completed hotel schema

--- unit_2/w05d05/homework/hotels_app/models/hotel.js
+++ unit_2/w05d05/homework/hotels_app/models/hotel.js
@@ -3,13 +3,33 @@ const Schema = mongoose.Schema
 
 const hotelSchema = new Schema (
   {
-
-
-
-  }
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    location: String,
+    rating: {
+      type: Number,
+      max: 5,
+    },
+    vacancies: {
+      type: Boolean,
+      default: true
+    },
+    tags: [],
+    rooms: [
+      {
+        roomNumber: Number,
+        size: String,
+        price: Number,
+        booked: Boolean
+      }
+    ]
+  },
   {timestamps: true}
 )
 
-const Hotel = mongoose.model('Hotel', tweetSchema)
+const Hotel = mongoose.model('Hotel', hotelSchema)
 
 module.export = Hotel

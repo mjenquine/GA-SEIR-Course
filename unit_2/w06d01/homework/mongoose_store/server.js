ed18e4739f971ce32fdd5477bb7ae3246bdc9338
Mark Jenquine <mjenquine@gmail.com>
2020-02-24 20:05:17 -0600

    started index.ejs file

---
+++ unit_2/w06d01/homework/mongoose_store/server.js
@@ -0,0 +1,48 @@
+//Variables
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const Product = require('./models/products.js')
+const PORT = 3000
+//Middleware
+app.use(express.urlencoded({exteded: true}))
+//Database connection
+mongoose.connect('mongodb://localhost:27017/basiccrud', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', ()=> {
+    console.log('connected to mongo');
+});
+//////////////////////Routes//////////////////////////
+
+//Presentation Rountes////////////////////////////////
+//INDEX
+app.get('/fruits', (req, res) => {
+  Product.find({}, (error, allProducts) => {
+    res.render('index.ejs', {
+      products: allProducts
+    })
+  })
+})
+//NEW
+app.get('/products/new', (req, res) => {
+  res.render('new.ejs')
+})
+//SHOW
+
+//EDIT
+
+//Functional Routes///////////////////////////////////
+
+//CREATE
+app.post('/products/', (req, res) => {
+  Product.create(req.body, (error, createdProduct) => {
+    res.redirect('/products')
+  })
+})
+//UPDATE
+
+//DELETE
+
+//Listen//////////////////////////////////////////////
+app.listen(PORT, (req, res) => {
+  console.log('listening...');
+})

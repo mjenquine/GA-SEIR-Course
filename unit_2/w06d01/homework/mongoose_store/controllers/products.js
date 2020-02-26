47017825f3323c721c81f71c80e8f4c67633776d
Mark Jenquine <mjenquine@gmail.com>
2020-02-25 23:20:32 -0600

    got the buy button to work

--- unit_2/w06d01/homework/mongoose_store/controllers/products.js
+++ unit_2/w06d01/homework/mongoose_store/controllers/products.js
@@ -1,1 +1,67 @@
 const router = require('express').Router()
+const Product = require('../models/products.js')
+
+//////////////////////Routes//////////////////////////
+
+//Presentation Rountes////////////////////////////////
+//INDEX
+router.get('/', (req, res) => {
+  Product.find({}, (error, allProducts) => {
+    res.render('index.ejs', {
+      products: allProducts
+    })
+  })
+})
+//NEW
+router.get('/new', (req, res) => {
+  res.render('new.ejs')
+})
+//SHOW
+router.get('/:id', (req, res) => {
+  Product.findById(req.params.id, (error, foundProduct) => {
+    res.render('show.ejs', {
+      product: foundProduct
+    })
+  })
+})
+//EDIT
+router.get('/:id/edit', (req, res) => {
+  Product.findById(req.params.id, (err, foundProduct) => {
+    res.render('edit.ejs', {
+      product: foundProduct
+    })
+  })
+})
+//Functional Routes///////////////////////////////////
+
+//CREATE
+router.post('/', (req, res) => {
+  Product.create(req.body, (error, createdProduct) => {
+    res.redirect('/products')
+  })
+})
+//UPDATE
+router.put('/buy/:id', (req, res) => {
+  Product.findByIdAndUpdate(req.params.id, {$inc: {qty: -1}}, (err, updatedProduct) => {
+    res.render('show.ejs', {
+      product: updatedProduct
+    })
+  })
+})
+router.put('/:id', (req, res) => {
+  Product.findByIdAndUpdate(req.params.id, req.body, (err, updatedProduct) => {
+    res.render('show.ejs', {
+      product: updatedProduct
+    })
+  })
+})
+//DELETE
+router.delete('/:id', (req, res) => {
+  Product.findByIdAndRemove(req.params.id, (error, deletedProduct) => {
+    res.redirect('/products')
+  })
+})
+
+
+
+module.exports = router
--- unit_2/w06d01/homework/mongoose_store/server.js
+++ unit_2/w06d01/homework/mongoose_store/server.js
@@ -3,69 +3,19 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const methodOverride = require('method-override');
-const Product = require('./models/products.js')
+const productsController = require('./controllers/products.js')
 const PORT = 3000
 //Middleware
 app.use(express.urlencoded({exteded: true}))
 app.use(methodOverride('_method'))
+
+app.use('/products', productsController)
 //Database connection
-mongoose.connect('mongodb://localhost:27017/basiccrud', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/basiccrud', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 mongoose.connection.once('open', ()=> {
     console.log('connected to mongo');
 });
-//////////////////////Routes//////////////////////////
 
-//Presentation Rountes////////////////////////////////
-//INDEX
-app.get('/products', (req, res) => {
-  Product.find({}, (error, allProducts) => {
-    res.render('index.ejs', {
-      products: allProducts
-    })
-  })
-})
-//NEW
-app.get('/products/new', (req, res) => {
-  res.render('new.ejs')
-})
-//SHOW
-app.get('/products/:id', (req, res) => {
-  Product.findById(req.params.id, (error, foundProduct) => {
-    res.render('show.ejs', {
-      product: foundProduct
-    })
-  })
-})
-//EDIT
-app.get('/products/:id/edit', (req, res) => {
-  Product.findById(req.params.id, (err, foundProduct) => {
-    res.render('edit.ejs', {
-      product: foundProduct
-    })
-  })
-})
-//Functional Routes///////////////////////////////////
-
-//CREATE
-app.post('/products/', (req, res) => {
-  Product.create(req.body, (error, createdProduct) => {
-    res.redirect('/products')
-  })
-})
-//UPDATE
-app.put('/products/:id', (req, res) => {
-  Product.findByIdAndUpdate(req.params.id, req.body, (err, updatedProduct) => {
-    res.render('show.ejs', {
-      product: updatedProduct
-    })
-  })
-})
-//DELETE
-app.delete('/products/:id', (req, res) => {
-  Product.findByIdAndRemove(req.params.id, (error, deletedProduct) => {
-    res.redirect('/products')
-  })
-})
 //Listen//////////////////////////////////////////////
 app.listen(PORT, (req, res) => {
   console.log('listening...');

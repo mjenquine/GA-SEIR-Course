55fd1da90623c68e422a4906688fc0e576441581
Mark Jenquine <mjenquine@gmail.com>
2020-02-25 23:35:20 -0600

    homework mvp complete. Committing fruits app too

--- unit_2/w06d01/student_labs/captains_log/server.js
+++ unit_2/w06d01/student_labs/captains_log/server.js
@@ -2,10 +2,12 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const methodOverride = require('method-override');
 const Log = require('./models/logs.js')
 const PORT = 3000
 //Middleware
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 //Database Connection
 mongoose.connect('mongodb://localhost:27017/captainlogcrud', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open', ()=> {
@@ -19,16 +21,28 @@ mongoose.connection.once('open', ()=> {
 
 //Index
 app.get('/logs', (req, res) => {
-  res.send('hi')
+  Log.find({}, (error, allLogs) => {
+    if (error) {
+      res.send('Ooops')
+    }
+    res.render('index.ejs', {
+    logs: allLogs
+    })
+  })
 })
-
 //New
 app.get('/logs/new', (req, res) => {
   res.render('new.ejs')
 })
 
 //Show
-
+app.get('/logs/:id', (req, res) => {
+    Log.findById(req.params.id, (error, foundLog) => {
+      res.render('show.ejs', {
+        log: foundLog
+      })
+    })
+  })
 
 //Edit
 
@@ -51,7 +65,11 @@ app.post('/logs/', (req, res) => {
 
 
 //Delete
-
+app.delete('/logs/:id', (req, res) => {
+  Log.findByIdAndRemove(req.params.id, (error, deletedFruit) => {
+    res.redirect('/logs')
+  })
+})
 
 ///////////////////////////////////////////////////////////////////
 //                   End Routes

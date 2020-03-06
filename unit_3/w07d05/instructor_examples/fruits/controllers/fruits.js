8f7d4a1eed4d1ac9bc8f7fbd22056bf79a99932d
Arthur <ma2bernier@gmail.com>
2020-03-06 09:57:54 -0500

    Update Fruits Starter Code

---
+++ unit_3/w07d05/instructor_examples/fruits/controllers/fruits.js
@@ -0,0 +1,118 @@
+// const express = require('express');
+// const router = express.Router();
+const router = require('express').Router();
+const  Fruit = require('../models/fruits.js')
+
+/* **********************************
+PRESENTATIONAL ROUTES
+***********************************/
+
+        // Index
+        router.get('/', (req, res) => {
+          // First argument is going to contain data if something went wrong (**error**, allFruits)
+          // Second argument will contain data if we got what we asked for (error, **allFruits**)
+          Fruit.find({}, (error, allFruits) => {
+            if (error){
+              res.send('OOPs you have boo boo')
+            }
+            console.log(allFruits)
+            res.render('index.ejs', {
+              fruits: allFruits
+            })// end the res.render
+          })// this is the fruits call back
+        }/*This is the end of the router.get callback*/)
+        /* Index Ends */
+
+        // New  is connected Create [ Create has the functionality]
+        router.get('/new', (req, res) => {
+          res.render('new.ejs')
+        })
+
+        // SEED
+        router.get('/seed', (req, res)=>{
+          Fruit.create([
+              {
+                  name:'grapefruit',
+                  color:'pink',
+                  readyToEat:true
+              },
+              {
+                  name:'grape',
+                  color:'purple',
+                  readyToEat:false
+              },
+              {
+                  name:'avocado',
+                  color:'green',
+                  readyToEat:true
+              }
+          ], (err, data)=>{
+              res.redirect('/fruits');
+          })
+      });
+
+        // Show
+        router.get('/:id', (req, res) => {
+          Fruit.findById(req.params.id, (error, foundFruit) => {
+            res.render('show.ejs', {
+              fruit: foundFruit
+            })
+          })
+        })
+
+        // Edit
+        router.get('/:id/edit', (req, res)=>{
+          Fruit.findById(req.params.id, (err, foundFruit)=>{ //find the fruit
+            res.render('edit.ejs', {
+            fruit: foundFruit //pass in found fruit
+        });
+    });
+});
+
+/*********************************
+     Presntational Routes End
+*********************************/
+
+/******************************************************************************/
+
+/*********************************
+     FUNCTIONAL ROUTES
+*********************************/
+        // ******CREATE *****
+        // Create is connected to New [New shows the form that allows you to call this Create Functionality]
+        router.post('/', (req, res)=>{
+            if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
+                req.body.readyToEat = true;
+            } else { //if not checked, req.body.readyToEat is undefined
+                req.body.readyToEat = false;
+            }
+            Fruit.create(req.body, (error, createdFruit) => {
+              res.redirect('/fruits');
+            })
+        });
+        /* End Create Route */
+
+        // Update
+        router.put('/:id', (req, res) => {
+          if (req.body.readyToEat === 'on'){
+              req.body.readyToEat = true;
+              } else {
+              req.body.readyToEat = false;
+              }
+              Fruit.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedFruit)=>{
+              res.redirect(`/fruits/${updatedFruit.id}`);
+          });
+        })
+
+        // Delete
+        router.delete('/:id', (req, res) => {
+          Fruit.findByIdAndRemove(req.params.id, (err, deletedFruit) => {
+            res.redirect('/fruits')
+          })
+        })
+
+/*********************************
+     FUNCTIONAL ROUTES End
+*********************************/
+
+module.exports = router;
---
+++ unit_3/w07d05/instructor_examples/fruits/server.js
@@ -0,0 +1,46 @@
+/******************************************************************
+  Initialize my Variables
+*****************************************************************/
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const fruitsController = require('./controllers/fruits.js');
+const PORT = 3000;
+/******************************************************************
+  End Variables
+******************************************************************/
+
+/********************************************************************
+  Express Middleware i.e
+  Body Parser, Static, Method Override etc
+*******************************************************************/
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));// _method is the query param we use to tell express.js what method we actually want to use
+app.use(express.static('public'));
+app.use('/fruits', fruitsController);
+/*********************************************************************
+  End My Middleware
+**********************************************************************/
+
+
+/******************************************************************
+Database Connection
+*****************************************************************/
+mongoose.connect('mongodb://localhost:27017/fruits1', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connection.once('open', () => {
+  console.log('connected to mogodb database.')
+})
+/******************************************************************
+  End Database Connection
+*****************************************************************/
+
+/******************************************************************
+  Listen
+*****************************************************************/
+  app.listen(PORT, (req, res) => {
+    console.log("Arthur is awesome...")
+  })
+  /******************************************************************
+    End Listen
+  *****************************************************************/

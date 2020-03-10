c4d2ad6fb5de4a9d133c0ac87c2da86d02beea39
Mark Jenquine <mjenquine@gmail.com>
2020-03-09 20:36:31 -0500

    got app rendering on the page

---
+++ unit_3/w08d01/homework/react_groceries/src/data.js
@@ -0,0 +1,24 @@
+const groceries = [
+  {
+  item: 'avocado',
+  brand: 'Avocados from Mexico',
+  units: '5',
+  quantity: 0,
+  isPurchased: false
+  },
+  {
+    item: 'Soy Milk',
+    brand: '365',
+    units: '1',
+    quantity: 0,
+    isPurchased: false
+  },
+  {
+    item: 'Peanuts',
+    brand: 'Planters',
+    units: '1',
+    quantity: 0,
+    isPurchased: false
+  }
+]
+export default groceries

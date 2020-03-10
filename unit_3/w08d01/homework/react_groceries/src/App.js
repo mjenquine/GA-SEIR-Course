710d4f62dfbce89ef989f3d49e084f81076f6634
Mark Jenquine <mjenquine@gmail.com>
2020-03-09 21:54:43 -0500

    handle change is working

--- unit_3/w08d01/homework/react_groceries/src/App.js
+++ unit_3/w08d01/homework/react_groceries/src/App.js
@@ -5,13 +5,86 @@ class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      groceries: groceries
+      groceries: groceries,
+      item: '',
+      brand: '',
+      units: '',
+      quantity: 0,
+      isPurchased: false
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(event) {
+    console.log(event.target.value);
+    this.setState({ [event.target.id]: event.target.value})
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    const newItem = {
+      item: this.state.item,
+      brand: this.state.brand,
+      units: this.state.units,
+      quantity: this.state.quantity,
+      isPurchased: this.state.isPurchased
+    }
+    this.setState({
+      groceries: [newItem, ...this.state.groceries],
+      item: '',
+      brand: '',
+      units: '',
+      quantity: 0,
+      isPurchased: false
+    })
   }
   render () {
     return (
       <div>
         <h1> Grocery List </h1>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="item">Item </label>
+          <input
+            type="text"
+            value={this.state.item}
+            onChange={this.handleChange}
+            id="item"
+          />
+          <br></br>
+          <label htmlFor="brand">Brand </label>
+          <input
+            type="text"
+            value={this.state.brand}
+            onChange={this.handleChange}
+            id="brand"
+          />
+          <br></br>
+          <label htmlFor="units">Purchase Unit </label>
+          <input
+            type="text"
+            value={this.state.units}
+            onChange={this.handleChange}
+            id="units"
+          />
+          <br></br>
+          <label htmlFor="quantity">Purchase Qty </label>
+          <input
+            type="text"
+            value={this.state.quantity}
+            onChange={this.handleChange}
+            id="quantity"
+          />
+          <br></br>
+          <label htmlFor="isPurchased">Purchased </label>
+          <input
+            type="text"
+            value={this.state.isPurchased}
+            onChange={this.handleChange}
+            id="isPurchased"
+          />
+          <br></br>
+          <input type="submit" />
+        </form>
+
         {this.state.groceries.map(grocery => grocery.isPurchased
           ? " "
           : <div>
@@ -21,10 +94,7 @@ class App extends Component {
               <div>Purchase Qty: {grocery.quantity}</div>
               <br/>
             </div>
-
         )}
-
-
       </div>
     )
   }

40772acf2b075b589811116a615c6f1e3d6ddbad
Mark Jenquine <mjenquine@gmail.com>
2020-03-11 11:58:20 -0500

    Created 3 Components

---
+++ unit_3/w08d03/student_labs/lifting-up-state-lab/src/components/CardContainer.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class CardContainer extends Component {
+  render () {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default CardContainer
---
+++ unit_3/w08d03/student_labs/lifting-up-state-lab/src/components/CardList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class CardList extends Component {
+  render () {
+    return (
+      <ul></ul>
+    )
+  }
+}
+
+export default CardList

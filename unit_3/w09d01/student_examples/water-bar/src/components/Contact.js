dd150d3ae9fdbeac57b8e8d59cb70e267416278d
Karolin Rafalski <kr2174@caa.columbia.edu>
2020-03-13 14:41:05 -0400

    add react router lesson

---
+++ unit_3/w09d01/student_examples/water-bar/src/components/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Contact extends React.Component {
+  render () {
+    return (
+      <div className="contact">
+        <h3>Contact Us</h3>
+        <main>
+          <h4>Message in a bottle:<br/> <span>Just put your message in a bottle and toss it into a body of water. Note: Response time is highly variable</span></h4>
+          <h4>Phone: <br/><span>Sorry, we're too cool to have a phone</span></h4>
+          <h4>email:<br/> <span>us@water.bar</span> </h4>
+        </main>
+      </div>
+    )
+  }
+}
+
+export default Contact

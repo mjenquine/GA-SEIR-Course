e4cb97ea44220515fc6798f2e04f71b532e26945
Brendan Quirk <brendanquirk@Brendans-MacBook-Pro.local>
2020-03-23 14:55:52 -0600

    fixing bonus2

---
+++ unit_4/w10d01/student_labs/bonus2.rb
@@ -0,0 +1,56 @@
+
+# Ruby enumerable diagnostic
+
+# Feel free to work on the problems in any order
+
+words = %w(We the People of the United States, in Order to form a more
+perfect Union, establish Justice, insure domestic
+Tranquility, provide for the common defence, promote the
+general Welfare, and secure the Blessings of Liberty to
+ourselves and our Posterity, do ordain and establish this
+Constitution for the United States of America.)
+
+even_nums = [0, 2, 4, 6]
+odd_nums = [1, 5, 7, 9]
+mixed_nums = [0, 2, 4, 7]
+
+
+  # Question 1
+  # Write ruby code to return the longest entry in the list supplied ( words )
+  # Assume each entry in the list implements a length method that returns
+  # a Fixnum.
+  def longest(list)
+    # Your code goes here
+  end
+
+  # Question 2
+  # Write ruby code to return the sum 1 to 99 (expect 4950)
+  # (i.e. apply the `+` operator).
+  # Assume each entry implements a `+` method.
+  def sum(list)
+    # Your code goes here
+  end
+
+  # Question 3
+  # Write ruby code that returns true if some of the entries
+  # in the list of numbers are odd (test the three given number arrays)
+  def some_odd(numbers)
+    # Your code goes here
+  end
+
+  # Question 4
+  # Write ruby code that returns true if every entry in the
+  # list of numbers are even (test the three given number arrays)
+  def every_even(numbers)
+    # Your code goes here
+  end
+
+  # Question 5
+  # Write ruby code that returns an array with all of the
+  # entries from the list of words capitalized (i.e. 'foo' becomes 'Foo').
+  def transform(words)
+    # Your code goes here
+  end
+
+  # Question 6
+  # Return to the previous day's labs and hw and work on any of the hungry for more questions

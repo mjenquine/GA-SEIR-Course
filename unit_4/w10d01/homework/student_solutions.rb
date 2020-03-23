ca4f9677cef0706125b4a6dd09a1af361f826cbd
Mark Jenquine <mjenquine@gmail.com>
2020-03-23 18:08:07 -0500

    completed enumerating on nums array

--- unit_4/w10d01/homework/student_solutions.rb
+++ unit_4/w10d01/homework/student_solutions.rb
@@ -1,9 +1,39 @@
-puts "Hello World!"
+# puts "Hello World!"
+#
+# adjective = "Big Bad"
+#
+# puts "Hello #{adjective} World!"
+#
+# greeting = "Hello World".upcase
+#
+# puts greeting
 
-adjective = "Big Bad"
+##############################################################
+##############    Nums Array and Enumerables   ###############
+##############################################################
 
-puts "Hello #{adjective} World!"
+nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0, 35, 19, 66, 3, 22, 21, 4, 4, 4, 0, 1, 2, 3, 4, 12, 15]
 
-greeting = "Hello World".upcase
-
-puts greeting
+# print nums.uniq
+# nums.push(5)
+# nums.pop
+# nums.shift
+# nums.unshift(5)
+# print nums.length
+# print nums
+# puts nums.include? 8
+# print nums.find_all { |i| i > 10}
+# puts nums.all? { |nums| nums > 0 }
+# puts nums.any? { |nums| nums % 8 == 0}
+# puts nums.count { |i| i > 4}
+# nums.each_with_index { |item, index|
+#   puts item * index
+# }
+# puts nums.find { |i| i > 0 and i % 7 == 0 and i % 5 == 0 }
+# puts nums.find_index { |i| i > 0 and i % 7 == 0 and i % 5 == 0 }
+# print nums.first(3)
+# print nums.last(5)
+# print nums.group_by { |i| i % 3}
+# puts nums.minmax
+# print nums.reject { |i| i % 3 == 0 }
+# print nums.select { |i| i % 5 == 0 }

8a25a7000b22d00e97ec2a42efb6fef786cc72aa
Ayla Ex <aylaex01@gmail.com>
2020-03-25 12:12:05 -0400

    added questions - prepped for review

---
+++ unit_4/w10d04/instructor_examples/nfl.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE nfl;
+\c nfl;
+\i nfl/nfl_seed_data/schema.sql;
+\i nfl/nfl_seed_data/players.sql;
+\i nfl/nfl_seed_data/teams.sql;
+
+---------------------------------------------------
+---------------------------------------------------
+-- 1. List the names of all NFL teams
+
+
+-- 2. List the stadium name and head coach of all NFC teams
+
+
+-- 3. List the head coaches of the AFC South
+
+
+-- 4. The total number of players in the NFL
+
+
+-- 5. The team names and head coaches of the NFC North and AFC East
+
+
+-- 6. The 50 players with the highest salaries
+
+
+-- 7. The average salary of all NFL players
+
+
+-- 8. The names and positions of players with a salary above 10_000_000
+
+
+-- 9. The player with the highest salary in the NFL
+
+
+-- 10. The name and position of the first 100 players with the lowest salaries
+
+
+-- 11. The average salary for a DE in the NFL

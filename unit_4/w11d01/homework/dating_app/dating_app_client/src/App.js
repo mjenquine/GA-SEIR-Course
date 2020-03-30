9a61476bf223264ecd89e75b6293062744b36efb
Mark Jenquine <mjenquine@gmail.com>
2020-03-30 16:56:09 -0500

    created react app

---
+++ unit_4/w11d01/homework/dating_app/dating_app_client/src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import Daters from './components/Hello'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className ="App">
+        <Daters />
+      </div>
+
+    )
+  }
+
+
+}
+
+export default App;

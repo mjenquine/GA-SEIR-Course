1bf5b8685e84da860638fd982381f262834d68f7
Mark Jenquine <mjenquine@gmail.com>
2020-03-30 20:48:42 -0500

    flexbox layout

--- unit_4/w11d01/homework/dating_app/dating_app_client/src/App.js
+++ unit_4/w11d01/homework/dating_app/dating_app_client/src/App.js
@@ -1,14 +1,26 @@
 import React, {Component} from 'react';
 import Daters from './components/Hello'
 import './App.css';
+import './index.css'
 
 class App extends Component {
+
+
   render() {
     return (
-      <div className ="App">
-        <Daters />
-      </div>
-
+      <>
+        <header>DATING APP</header>
+        <div className="daters-main">
+          <aside className="daters">
+            <Daters />
+          </aside>
+            <main></main>
+          <aside className="daters">  
+            <Daters />
+          </aside>
+        </div>
+        <footer></footer>
+      </>
     )
   }
 
--- unit_4/w11d01/homework/dating_app/dating_app_client/src/components/Hello.js
+++ unit_4/w11d01/homework/dating_app/dating_app_client/src/components/Hello.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 
 class Daters extends Component {
+  state = {
+    daters: []
+  }
+  componentDidMount() {
+    this.getDaters()
+  }
   getDaters () {
-    fetch('localhost:3000')
-    .then( daters => console.log(daters))
+    fetch('http://localhost:3000/users'
+  )
+    .then(res => res.json())
+    .then(jsonedDaters => this.setState({daters: jsonedDaters}))
+    .catch( error => console.error(error))
   }
   render () {
     return (
-      <h1>Hello Dating World</h1>
+      <>
+        {this.state.daters.map( dater => {
+          return (
+            <div key={dater.id}>
+              <h3>Name: {dater.name} </h3>
+              <img src={dater.img} alt={dater.name}/>
+              <h4>Starsign: {dater.starsign} </h4>
+              <h4>Age: {dater.age} </h4>
+              {dater.ltl
+                ? <h5> Loves to laugh and have a good time</h5>
+                : <h5> Hates laughing, does not like having a good time</h5>
+              }
+            </div>
+          )
+        })}
+      </>
     )
   }
 }
